e6ca8e1085bc5360a2f7459d37c21089d99b3395
vins <sumbaluwudevin@gmail.com>
2022-06-15 22:02:52 +0700

    handling duplicate email or password in testing code

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,9 +1,13 @@
 class Volunteer < ApplicationRecord
     validates :org_name, presence: true
-    validates :org_email, presence: true, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }
+    validates :org_email, presence: true, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }, uniqueness: true
     validates :org_phone, presence: true
     validates :org_address, presence: true
-    validates :org_pass, presence: true
+    validates :org_pass, presence: true, uniqueness: true
+
+    def method_name
+        
+    end
     
 
 
--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -84,4 +84,48 @@ RSpec.describe Volunteer, type: :model do
   end
   
 
+  it "should require an unique email" do
+    volunteer = Volunteer.create(
+      org_name: 'Bersih-bersih',
+      org_email: 'volunteerbb2be@gmail.com',
+      org_phone: '08136789002',
+      org_address: 'Jl. Mulawarman no 19',
+      org_pass: '1234'
+    )
+
+    volunteer2 = Volunteer.new(
+      org_name: 'Halo sampah',
+      org_email: 'volunteerbb2be@gmail.com',
+      org_phone: '0812313123',
+      org_address: 'Jl. Mulawarman no 100',
+      org_pass: '1234567'
+    )
+
+    volunteer2.valid?
+
+    expect(volunteer2.errors[:org_email]).to include("has already been taken")
+  end
+
+  it "should require a unique password" do
+    volunteer = Volunteer.create(
+      org_name: 'Bersih-bersih',
+      org_email: 'volunteerbb2be@gmail.com',
+      org_phone: '08136789002',
+      org_address: 'Jl. Mulawarman no 19',
+      org_pass: '1234'
+    )
+
+    volunteer2 = Volunteer.new(
+      org_name: 'Halo sampah',
+      org_email: 'sampah@gmail.com',
+      org_phone: '0812313123',
+      org_address: 'Jl. Mulawarman no 100',
+      org_pass: '1234'
+    )
+
+    volunteer2.valid?
+
+    expect(volunteer2.errors[:org_pass]).to include("has already been taken")
+  end
+
 end
