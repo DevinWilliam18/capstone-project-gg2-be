4c24c491b51c8c296a953caef68303265d796b0b
vins <sumbaluwudevin@gmail.com>
2022-06-17 21:23:20 +0700

    perform 'FactoryBot' and 'Faker' inside the test code

--- spec/factories/volunteers.rb
+++ spec/factories/volunteers.rb
@@ -1,3 +1,9 @@
-RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
+FactoryBot.define do
+  factory :volunteer do
+    org_name { Faker::Name.name_with_middle }
+    org_email {'prabuneno@gmail.com'}
+    org_phone { Faker::PhoneNumber.cell_phone_with_country_code }
+    org_address { Faker::Address.full_address }
+    org_pass { Faker::Alphanumeric.alphanumeric }
+  end
 end
