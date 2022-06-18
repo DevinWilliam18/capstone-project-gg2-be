ea2aa32a2d7480ee949a059fdf7624081e6b365e
vins <sumbaluwudevin@gmail.com>
2022-06-18 15:09:36 +0700

    add 'Order' model

---
+++ db/migrate/20220618080825_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.datetime :order_time
+      t.string :status
+      t.datetime :finished_time
+      t.references :volunteers, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/orders.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :order do
+    order_time { "2022-06-18 15:08:25" }
+    status { "MyString" }
+    finished_time { "2022-06-18 15:08:25" }
+    volunteers { nil }
+  end
+end
