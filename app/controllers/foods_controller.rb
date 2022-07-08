c62462b5643c7ec7a406524c95fdc9341c9d8ebe
vins <sumbaluwudevin@gmail.com>
2022-07-08 16:03:50 +0700

    modify Food's controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,16 +1,28 @@
 class FoodsController < ApplicationController
-    before_action :get_resto, only: [:show, :create]
+    before_action :get_resto
     before_action :set_food, only: [:show, :update, :destroy]
 
     def index
-        @foods = Food.all
+        @foods = nil
         
-        if params[:letter].is_a?(Integer)
-            #should add join table
-            @foods = Food.by_id_province(params[:letter])
-        elsif params[:letter].is_a?(String)
-            @foods = Food.by_name(params[:letter])
+        if @resto.nil?
+            @foods = Food.all
+            if params[:letter].is_a?(Integer)
+                #should add join table
+                @foods = Food.by_id_province(params[:letter])
+            elsif params[:letter].is_a?(String)
+                @foods = Food.by_name(params[:letter])
+            end
+        else
+            @foods = @resto.foods
+            if params[:letter].is_a?(Integer)
+                #should add join table
+                @foods = Food.by_id_province(params[:letter])
+            elsif params[:letter].is_a?(String)
+                @foods = Food.by_name(params[:letter])
+            end
         end
+        
 
        render json: @foods
     end
@@ -31,7 +43,6 @@ class FoodsController < ApplicationController
         else
             render json: @food.errors, status: :unprocessable_entity 
         end
-        
     end
 
     def update
@@ -50,17 +61,29 @@ class FoodsController < ApplicationController
 
     private
     def get_resto
-        @resto = GarbageProducer.find(params[:garbage_producer_id])
+        @resto = nil
+        if params[:garbage_producer_id].nil?
+            @resto = nil
+        else
+            @resto = GarbageProducer.find(params[:garbage_producer_id])
+        end
+        
     end
 
      def set_food
-        @food = @resto.foods.find(params[:id])
+        if @resto.nil?
+            @food = nil
+        else
+            @food = @resto.foods.find(params[:id])
+        end
+
      end
 
      def food_params
         params.require(:food).permit(:food_name, :food_quantity, :food_desc, :expiration_date, :condition_id)
      end
      
+
 end
 
 
